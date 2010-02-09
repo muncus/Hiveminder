36b21e562a2150bff435aa925bc9d559c5589d97
Marc Dougherty <muncus@gmail.com>
2010-02-08 23:23:55 -0800

    logging cleanups in the task adapter
    now that it works, theres no need for so much logging!

--- src/org/nerdcircus/android/hiveminder/TaskAdapter.java
+++ src/org/nerdcircus/android/hiveminder/TaskAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 
+
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -27,27 +28,11 @@ public class TaskAdapter extends ArrayAdapter<Task>
         mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    /*
-    public void bindView(View view, Context context, Task task){
-        CheckBox desc = (CheckBox) view.findViewById(R.id.checkbox);
-        desc.setText(task.getSummary());
-        desc.setChecked(task.complete);
-        Log.d(TAG, "in bind view");
-        if(task.complete == true){
-            Log.d(TAG, "should be checked!");
-        }
-
-    }
-    */
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        Log.d(TAG, "in get view");
         View v = convertView;
         if(v == null){
-            Log.d(TAG, "null convert view");
             v = mInflater.inflate(R.layout.taskitem, null);
-            Log.d(TAG, "Inflate WORKED!");
             Log.d(TAG, v.toString());
         }
         Task task = this.getItem(position);
@@ -55,15 +40,9 @@ public class TaskAdapter extends ArrayAdapter<Task>
         Log.d(TAG, v.toString());
 
         
-        Log.d(TAG, "attempting fvbi()");
         CheckBox cb = (CheckBox) v.findViewById(R.id.checkbox);
-        //CheckBox cb = (CheckBox) ((ViewGroup)v).getChildAt(0); //XXX: we only have one child, for now.
-        Log.d(TAG, "done with fvbi()");
         cb.setText(task.getSummary());
         cb.setChecked(task.complete);
-        if(task.complete == true){
-            Log.d(TAG, "should be checked!");
-        }
 
         return v;
     }
