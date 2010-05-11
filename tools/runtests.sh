a5c311d44b8178121500bbf84c481b7d7ed285e3
Marc Dougherty <muncus@gmail.com>
2010-05-11 15:44:48 +0100

    release v6

--- tools/runtests.sh
+++ tools/runtests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-#ant install
-#cd tests && ant install
-#cd ..
 # script to run tests.
+
+ant install
+cd tests && ant install
+cd ..
 
 adb shell am instrument -w \
     org.nerdcircus.android.hiveminder.tests/android.test.InstrumentationTestRunner
